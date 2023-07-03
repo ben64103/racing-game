ccfac6c6c897c22da0a3df2cb0fea9884a672cc6
Benjamin Oluwadarasimi <benabolade@gmail.com>
2023-07-03 08:06:06 +0100

    👀👀

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-window.addEventListener("DOMContentLoaded", function () {
-  create();
-});
+const canvas = document.getElementById('canvas')
+
+canvas.contentEditable = true;
+let value
+canvas.addEventListener('keydown', function(e){
+  if((e.key === 'ArrowUp' && e.key === 'Arrowown' && e.key === 'ArrowLeft' && e.key === 'ArrowDown' ) && canvas.contentEditable === true) {
+    alert(`the key value is ${e.key}`)
+  }
+})
+  // The preferences and the info about documentation
+
